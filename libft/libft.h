7f403e2c6d81236c8591aa1906b8e2c0ce045de4
Camille Boucher <238995038+camilleboucher@users.noreply.github.com>
2025-10-20 12:39:01 +0200

    Starting libft with bitwise operators as much as possible to better understand the memory, hexa and binnary

---
+++ libft/libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Camille <private_mail>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/20 10:14:22 by Camille           #+#    #+#             */
+/*   Updated: 2025/10/20 11:54:08 by Camille          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+
+#endif
