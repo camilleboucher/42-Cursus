4253e290aeb18d131c7126e347a4165e7fbf6a1d
Camille Boucher <camilleboucher@users.noreply.github.com>
2026-02-12 11:02:03 +0100

    adding libft

---
+++ M1 - ft_printf/libft/ft_atoi.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Camille <private_mail>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/29 09:57:44 by Camille           #+#    #+#             */
+/*   Updated: 2025/10/29 10:48:22 by Camille          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_isspace(char c)
+{
+	return (c == ' ' || (c >= '\t' && c <= '\r'));
+}
+
+int	ft_atoi(const char *nptr)
+{
+	int	nbr;
+	int	sign;
+
+	nbr = 0;
+	sign = 1;
+	while (ft_isspace(*nptr))
+		nptr++;
+	if (*nptr == '-' || *nptr == '+')
+	{
+		if (*nptr == '-')
+			sign = -1;
+		nptr++;
+	}
+	while (ft_isdigit(*nptr))
+	{
+		nbr *= 10;
+		nbr += *nptr - '0';
+		nptr++;
+	}
+	return (nbr * sign);
+}
